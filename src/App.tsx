62a94ad35e10e338bd6c88841f073ae56ac5be1c
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2022-08-28 11:36:37 -0400

    (feat):"Create component header and logo"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import {Header} from "./components/Header";
+import './global.css'
 import './App.css'
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+      <div>
+        <Header />
+      </div>
   )
 }
 
